b1f08583f67e410bc6158c9c2117f0490e129a3e
dmytro-liashok <90052946+dmytro-liashok@users.noreply.github.com>
2023-12-03 20:55:38 +0000

    Deploying to gh-pages from @ dmytro-liashok/test-task@a9c12baf4123ea304206fc5e985d1ee9e9e1bd4b 🚀

--- static/js/81.4b2a4de0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task=self.webpackChunktest_task||[]).push([[81],{81:(e,s,a)=>{a.r(s),a.d(s,{default:()=>h});const t="Home_homePage__esogt",c="Home_homeTitle__0vDaF";var o=a(184);const h=()=>(0,o.jsx)("div",{className:t,children:(0,o.jsx)("h1",{className:c,children:"Choose a car at RentalAuto"})})}}]);
-//# sourceMappingURL=81.4b2a4de0.chunk.js.map
---
+++ static/js/81.6fc723bc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_task=self.webpackChunktest_task||[]).push([[81],{9081:(e,s,a)=>{a.r(s),a.d(s,{default:()=>h});const t="Home_homePage__esogt",c="Home_homeTitle__0vDaF";var o=a(184);const h=()=>(0,o.jsx)("div",{className:t,children:(0,o.jsx)("h1",{className:c,children:"Choose a car at RentalAuto"})})}}]);
+//# sourceMappingURL=81.6fc723bc.chunk.js.map
